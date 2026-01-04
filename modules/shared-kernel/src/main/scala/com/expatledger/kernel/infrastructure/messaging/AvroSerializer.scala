8d76ce050c762f795999fefc6b988529928bc81f
Alexander Vivas <falexvr@gmail.com>
2026-01-04 12:52:23 +0100

    refactor: Replace `SpecificDatumWriter` with `GenericDatumWriter` in `AvroSerializer`

--- modules/shared-kernel/src/main/scala/com/expatledger/kernel/infrastructure/messaging/AvroSerializer.scala
+++ modules/shared-kernel/src/main/scala/com/expatledger/kernel/infrastructure/messaging/AvroSerializer.scala
@@ -1,16 +1,16 @@
 package com.expatledger.kernel.infrastructure.messaging
 
 import org.apache.avro.Schema
-import org.apache.avro.generic.GenericRecord
+import org.apache.avro.generic.{GenericDatumWriter, GenericRecord}
 import org.apache.avro.io.EncoderFactory
-import org.apache.avro.specific.SpecificDatumWriter
+
 import java.io.ByteArrayOutputStream
 
 object AvroSerializer:
   def serialize(record: GenericRecord, schema: Schema): Array[Byte] =
     val out = new ByteArrayOutputStream()
     try {
-      val writer = new SpecificDatumWriter[GenericRecord](schema)
+      val writer = new GenericDatumWriter[GenericRecord](schema)
       val encoder = EncoderFactory.get().binaryEncoder(out, null)
       writer.write(record, encoder)
       encoder.flush()
